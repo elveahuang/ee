c1c67144977abf4164a34f4b74cda8571a8956c1
Elvea Huang <elvea.huang@gmail.com>
2025-06-20 19:08:14 +0800

    初始GlobalRequestFilter

---
+++ platform-commons/commons-core/src/main/java/cc/elvea/platform/commons/utils/MDCUtils.java
@@ -0,0 +1,24 @@
+package cc.elvea.platform.commons.utils;
+
+import org.slf4j.MDC;
+
+/**
+ * @author elvea
+ */
+public abstract class MDCUtils {
+
+    private static final String REQUEST_ID_KEY = "requestId";
+
+    public static void setRequestId(String requestId) {
+        MDC.put(REQUEST_ID_KEY, requestId);
+    }
+
+    public static String getRequestId() {
+        return MDC.get(REQUEST_ID_KEY);
+    }
+
+    public static void clear() {
+        MDC.clear();
+    }
+
+}
