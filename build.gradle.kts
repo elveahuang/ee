37bcfb7f42f7b0cda8c84bcb00908977b381a104
Elvea Huang <elvea.huang@gmail.com>
2025-07-31 11:29:26 +0800

    简化Maven仓库配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,9 @@ allprojects {
     repositories {
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/spring") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/spring-plugin") }
     }
 
     configure<JavaPluginExtension> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,11 +4,8 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        maven { url = uri("https://maven.aliyun.com/repository/spring") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/spring-plugin") }
     }
 }
 rootProject.name = "ee"
