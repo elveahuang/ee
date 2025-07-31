b54895055046437bc165f11dd705cf2e785d98e2
Elvea Huang <elvea.huang@gmail.com>
2025-07-31 11:37:00 +0800

    增加腾讯云镜像

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ allprojects {
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public") }
     }
 
     configure<JavaPluginExtension> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public") }
     }
 }
 rootProject.name = "ee"
