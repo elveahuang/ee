669620d9e410e72329b34af1f59bca4d8502a15e
Elvea Huang <elvea.huang@gmail.com>
2025-08-21 10:34:07 +0800

    精简依赖，本地原生动态库太大了，默认超过1G

--- platform-commons/commons-javacv/build.gradle.kts
+++ platform-commons/commons-javacv/build.gradle.kts
@@ -8,21 +8,22 @@ dependencies {
         exclude(group = "org.bytedeco", module = "artoolkitplus")
         exclude(group = "org.bytedeco", module = "flycapture")
         exclude(group = "org.bytedeco", module = "libdc1394")
-        exclude(group = "org.bytedeco", module = "libfreenect2")
         exclude(group = "org.bytedeco", module = "libfreenect")
+        exclude(group = "org.bytedeco", module = "libfreenect2")
         exclude(group = "org.bytedeco", module = "librealsense")
         exclude(group = "org.bytedeco", module = "librealsense2")
         exclude(group = "org.bytedeco", module = "opencv")
         exclude(group = "org.bytedeco", module = "openblas")
+        exclude(group = "org.bytedeco", module = "videoinput")
     }
     // javacpp
     api(group = "org.bytedeco", name = "javacpp-platform", version = javaCppVersion) {
         exclude(group = "org.bytedeco", module = "javacpp")
     }
     api(group = "org.bytedeco", name = "javacpp", version = javaCppVersion)
-    api(group = "org.bytedeco", name = "javacpp", version = javaCppVersion, classifier = "windows-x86_64")
     api(group = "org.bytedeco", name = "javacpp", version = javaCppVersion, classifier = "linux-x86_64")
     api(group = "org.bytedeco", name = "javacpp", version = javaCppVersion, classifier = "macosx-arm64")
+    api(group = "org.bytedeco", name = "javacpp", version = javaCppVersion, classifier = "windows-x86_64")
 
     // ffmpeg
     api(group = "org.bytedeco", name = "ffmpeg-platform", version = ffmpegVersion) {
@@ -30,9 +31,9 @@ dependencies {
         exclude(group = "org.bytedeco", module = "javacpp-platform")
     }
     api(group = "org.bytedeco", name = "ffmpeg", version = ffmpegVersion)
-    api(group = "org.bytedeco", name = "ffmpeg", version = ffmpegVersion, classifier = "windows-x86_64")
     api(group = "org.bytedeco", name = "ffmpeg", version = ffmpegVersion, classifier = "linux-x86_64")
     api(group = "org.bytedeco", name = "ffmpeg", version = ffmpegVersion, classifier = "macosx-arm64")
+    api(group = "org.bytedeco", name = "ffmpeg", version = ffmpegVersion, classifier = "windows-x86_64")
 
     // tesseract
     api(group = "org.bytedeco", name = "tesseract-platform", version = tesseractVersion) {
@@ -41,9 +42,9 @@ dependencies {
         exclude(group = "org.bytedeco", module = "leptonica-platform")
     }
     api(group = "org.bytedeco", name = "tesseract", version = tesseractVersion)
-    api(group = "org.bytedeco", name = "tesseract", version = tesseractVersion, classifier = "windows-x86_64")
     api(group = "org.bytedeco", name = "tesseract", version = tesseractVersion, classifier = "linux-x86_64")
     api(group = "org.bytedeco", name = "tesseract", version = tesseractVersion, classifier = "macosx-arm64")
+    api(group = "org.bytedeco", name = "tesseract", version = tesseractVersion, classifier = "windows-x86_64")
 
     // leptonica
     api(group = "org.bytedeco", name = "leptonica-platform", version = leptonicaVersion) {
@@ -51,7 +52,7 @@ dependencies {
         exclude(group = "org.bytedeco", module = "javacpp-platform")
     }
     api(group = "org.bytedeco", name = "leptonica", version = leptonicaVersion)
-    api(group = "org.bytedeco", name = "leptonica", version = leptonicaVersion, classifier = "windows-x86_64")
     api(group = "org.bytedeco", name = "leptonica", version = leptonicaVersion, classifier = "linux-x86_64")
     api(group = "org.bytedeco", name = "leptonica", version = leptonicaVersion, classifier = "macosx-arm64")
+    api(group = "org.bytedeco", name = "leptonica", version = leptonicaVersion, classifier = "windows-x86_64")
 }
