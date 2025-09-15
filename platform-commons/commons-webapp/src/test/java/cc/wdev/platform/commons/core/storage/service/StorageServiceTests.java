c56123495824d53005b696b3792ec280f90a675a
Elvea Huang <elvea.huang@gmail.com>
2025-09-15 10:17:29 +0800

    存储模块重构

--- platform-commons/commons-webapp/src/test/java/cc/wdev/platform/commons/core/storage/service/StorageServiceTests.java
+++ platform-commons/commons-webapp/src/test/java/cc/wdev/platform/commons/core/storage/service/StorageServiceTests.java
@@ -2,7 +2,9 @@ package cc.wdev.platform.commons.core.storage.service;
 
 import cc.wdev.dev.webapp.BaseTests;
 import cc.wdev.platform.commons.core.storage.StorageFactory;
+import cc.wdev.platform.commons.core.storage.StorageService;
 import cc.wdev.platform.commons.core.storage.aws.AwsStorageService;
+import cc.wdev.platform.commons.core.storage.cos.CosStorageService;
 import cc.wdev.platform.commons.core.storage.model.FileObject;
 import cc.wdev.platform.commons.core.storage.oss.OssStorageService;
 import org.junit.jupiter.api.Assertions;
@@ -21,6 +23,22 @@ public class StorageServiceTests extends BaseTests {
     @Test
     public void awsStorageServiceTest() throws Exception {
         AwsStorageService service = this.storage.getAwsStorageService();
+        this.baseTest(service);
+    }
+
+    @Test
+    public void ossStorageServiceTest() throws Exception {
+        OssStorageService service = this.storage.getOssStorageService();
+        this.baseTest(service);
+    }
+
+    @Test
+    public void cosStorageServiceTest() throws Exception {
+        CosStorageService service = this.storage.getCosStorageService();
+        this.baseTest(service);
+    }
+
+    private void baseTest(StorageService<?> service) throws Exception {
         Assertions.assertNotNull(service);
 
         ClassPathResource resource = new ClassPathResource("html/tpl.html");
@@ -30,23 +48,6 @@ public class StorageServiceTests extends BaseTests {
         String key = uploadFileObject.getKey();
         FileObject<?> getFileObject = service.getFile(key);
         Assertions.assertNotNull(getFileObject.getObject());
-
-        FileObject<?> getUrlObject = service.getUrl(key);
-        Assertions.assertNotNull(getUrlObject.getUrl());
-    }
-
-    @Test
-    public void ossStorageServiceTest() throws Exception {
-        OssStorageService service = this.storage.getOssStorageService();
-        Assertions.assertNotNull(service);
-
-        ClassPathResource resource = new ClassPathResource("html/tpl.html");
-        FileObject<?> uploadFileObject = service.uploadFile(resource.getFile());
-        Assertions.assertNotNull(uploadFileObject);
-
-        String key = uploadFileObject.getKey();
-        FileObject<?> getFileObject = service.getFile(key);
-        Assertions.assertNotNull(getFileObject.getUrl());
 
         FileObject<?> getUrlObject = service.getUrl(key);
         Assertions.assertNotNull(getUrlObject.getUrl());
