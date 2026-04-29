625efc8df138457f42d94665a61556ef2aeae109
Elvea Huang <elvea.huang@gmail.com>
2026-04-29 18:36:06 +0800

    Gradle buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
 }
 
 repositories {
-    maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public") }
+    mavenCentral()
+    gradlePluginPortal()
     maven { url = uri("https://maven.aliyun.com/repository/public") }
     maven { url = uri("https://maven.aliyun.com/repository/google") }
     maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-    mavenCentral()
-    gradlePluginPortal()
+    maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public") }
 }
 
 dependencies {
